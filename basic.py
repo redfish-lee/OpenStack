c4b3cee3a5d666d49a65e8f39c6493f955abcd84
Akii <akii@macdeMacBook-Air.local>
2016-05-10 00:35:15 +0800

    update bug

--- basic.py
+++ basic.py
@@ -2,11 +2,11 @@
 # -*- coding: UTF-8 -*-
 
 import shlex, subprocess, os, sys
-import config
-import func
+from config import *
+from func import *
 
 def add_hosts():
-  try:
+  
     print "[INFO] add ip on hosts"
     f = open(Hosts.HOSTS_DIR, 'a')
     original = sys.stdout
@@ -22,11 +22,9 @@ def add_hosts():
     # Only on stdout
     # sys.stdout = original
     f.close()
-  except:
-    print "[ERROR] adding ip to hosts"
 
 def install_basic():
-  try:
+  
     print "[INFO] installing epel"
     cmd = "yum install http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-6.noarch.rpm -y"
     subprocess.call(cmd.split())
@@ -42,58 +40,54 @@ def install_basic():
     print "[INFO] installing mariadb"
     cmd = "yum update -y"
     subprocess.call(cmd.split())
-  except:
-    print "[ERROR] installing basic env"
 
 def setup_mariadb_config():
-  try:
-    print "[INFO] copy mariadb config file"
-    srcFile = "./lib/mariadb/mariadb_openstack.cnf "
-    dstFile = "/etc/my.cnf.d/mariadb_openstack.cnf "
-    cmd = "cp " + srcFile + dstFile + "-i"
-    subprocess.call(cmd.split())
+  
+  print "[INFO] copy mariadb config file"
+  srcFile = "./lib/mariadb/mariadb_openstack.cnf "
+  dstFile = "/etc/my.cnf.d/mariadb_openstack.cnf "
+  cmd = "cp " + srcFile + dstFile + "-i"
+  subprocess.call(cmd.split())
 
-    print "[INFO] update controller ip"
-    func.inplace_change(dstFile, 'CONTROLLER_IP', Hosts.HOSTS_IP[Agent.CONTROLLER])
+  print "[INFO] update controller ip"
+  func.inplace_change(dstFile, 'CONTROLLER_IP', Hosts.HOSTS_IP[Agent.CONTROLLER])
 
-    print "[INFO] enable mariadb service"
-    cmd = "systemctl enable mariadb.service"
-    subprocess.call(cmd.split())
+  print "[INFO] enable mariadb service"
+  cmd = "systemctl enable mariadb.service"
+  subprocess.call(cmd.split())
 
-    print "[INFO] start mariadb service"
-    cmd = "systemctl start mariadb.service"
-    subprocess.call(cmd.split())
+  print "[INFO] start mariadb service"
+  cmd = "systemctl start mariadb.service"
+  subprocess.call(cmd.split())
 
-    print "[INFO] mysql secure installation"
-    cmd = "mysql_secure_installation"
-    subprocess.call(cmd.split())
-  except:
-    print "[ERROR] setup mariadb config"
+  print "[INFO] mysql secure installation"
+  cmd = "mysql_secure_installation"
+  subprocess.call(cmd.split())
+  
 
 def install_rabbitmq():
-  try:
-    print "[INFO] installing rabbitmq server"
-    cmd = "yum install rabbitmq-server -y"
-    subprocess.call(cmd.split())
+  
+  print "[INFO] installing rabbitmq server"
+  cmd = "yum install rabbitmq-server -y"
+  subprocess.call(cmd.split())
 
-    print "[INFO] enable rabbitmq server service"
-    cmd = "systemctl enable rabbitmq-server.service"
-    subprocess.call(cmd.split())
+  print "[INFO] enable rabbitmq server service"
+  cmd = "systemctl enable rabbitmq-server.service"
+  subprocess.call(cmd.split())
 
-    print "[INFO] start rabbitmq server service"
-    cmd = "systemctl start rabbitmq-server.service"
-    subprocess.call(cmd.split())
+  print "[INFO] start rabbitmq server service"
+  cmd = "systemctl start rabbitmq-server.service"
+  subprocess.call(cmd.split())
 
-    print "[INFO] start rabbitmq server service"
-    # rabbitmqctl add_user openstack RABBIT_PASS
-    cmd = "rabbitmqctl add_user " + User.RABBITMQ[ACCOUNT] + " " + User.RABBITMQ[PASSWORD]
-    subprocess.call(cmd.split())
-    
-    print "[INFO] start rabbitmq server service"
-    cmd = "rabbitmqctl set_permissions " + User.RABBITMQ[ACCOUNT] + " '.*' '.*' '.*'"
-    subprocess.call(cmd.split())
-  except:
-    print "[ERROR] installing rabbitmq server"
+  print "[INFO] start rabbitmq server service"
+  # rabbitmqctl add_user openstack RABBIT_PASS
+  cmd = "rabbitmqctl add_user " + User.RABBITMQ[ACCOUNT] + " " + User.RABBITMQ[PASSWORD]
+  subprocess.call(cmd.split())
+  
+  print "[INFO] start rabbitmq server service"
+  cmd = "rabbitmqctl set_permissions " + User.RABBITMQ[ACCOUNT] + " '.*' '.*' '.*'"
+  subprocess.call(cmd.split())
+  
 
 
 def main():
